68dd18120863db0f934597911c0765fac785e7b6
Jan-Kees van Andel <jankeesvanandel@gmail.com>
2015-11-04 22:12:11 +0100

    Changed to AWS Elastic IP

--- publish_to_aws.sh
+++ publish_to_aws.sh
@@ -5,9 +5,12 @@ grunt build
 echo "Publishing to AWS"
 rm dist.zip
 zip -r dist.zip dist
-AWS_HOST=ec2-52-31-48-79.eu-west-1.compute.amazonaws.com
+
+AWS_HOST=ec2-52-31-48-218.eu-west-1.compute.amazonaws.com
 AWS_USER=ec2-user
 AWS_KEY=~/.ssh/DevoxxWallKeyPair2015.pem
+
 scp -i $AWS_KEY dist.zip $AWS_USER@$AWS_HOST:~
 ssh -i $AWS_KEY $AWS_USER@$AWS_HOST "rm -rf /www/data/dist_new/* && unzip ~/dist.zip -d /www/data/dist_new && rm -rf /www/data/dist/* && mv -f /www/data/dist_new/dist /www/data"
+
 echo "Done"
