1c267be3d3d800ebfa421a62613a3c1a7637a987
Jan-Kees van Andel <jankeesvanandel@gmail.com>
2015-11-09 13:50:44 +0100

    Added polling of voting results

--- app/scripts/wall/voting/VotingCtrl.js
+++ app/scripts/wall/voting/VotingCtrl.js
@@ -1,15 +1,17 @@
 "use strict";
 
-wallApp.controller('VotingCtrl', [ "$timeout", "VotingService", function ($timeout, VotingService) {
+wallApp.controller('VotingCtrl', [ "$timeout", "$interval", "VotingService", function ($timeout, $interval, VotingService) {
     var self = this;
     self.loadingWeek = true;
     self.loadingDay = true;
-    VotingService.topOfWeek().then(function (data) {
-        self.topTalksOfWeek = data.talks;
-        self.loadingWeek = false;
-    });
-    VotingService.topOfDay().then(function (data) {
-        self.topTalksOfDay = data.talks;
-        self.loadingDay = false;
-    });
+    $interval(function () {
+        VotingService.topOfWeek().then(function (data) {
+            self.topTalksOfWeek = data.talks;
+            self.loadingWeek = false;
+        });
+        VotingService.topOfDay().then(function (data) {
+            self.topTalksOfDay = data.talks;
+            self.loadingDay = false;
+        });
+    }, 10000);
 }]);
