98456cf73f0dc8d7274b1ce00dfdcd36a477e463
Jan-Kees van Andel <jankeesvanandel@gmail.com>
2015-11-11 13:13:12 +0100

    fixed missing wednesday in voting day list

--- app/scripts/wall/voting/votingservice.js
+++ app/scripts/wall/voting/votingservice.js
@@ -1,11 +1,7 @@
-"use strict";
-
 wallApp.factory('VotingService', ['$http', '$q', function($http, $q) {
 
-    var dayNumberToName = ['sunday', 'monday', 'tuesday', 'thursday', 'friday', 'saturday'];
-    var dayNumber = new Date().getDay();
+    var dayNumberToName = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
     var topOfWeekUrl = 'https://api-voting.devoxx.com/DV15/top/talks?limit=5';
-    var topOfDayUrl = 'https://api-voting.devoxx.com/DV15/top/talks?limit=5&day=' + dayNumberToName[dayNumber];
 
     return {
         topOfWeek: function() {
@@ -21,6 +17,9 @@ wallApp.factory('VotingService', ['$http', '$q', function($http, $q) {
         },
         topOfDay : function() {
             var defer = $q.defer();
+
+            var dayNumber = new Date().getDay();
+            var topOfDayUrl = 'https://api-voting.devoxx.com/DV15/top/talks?limit=5&day=' + dayNumberToName[dayNumber];
 
             $http.get(topOfDayUrl).success(function(data, status, headers, config) {
                 defer.resolve(data);
