78d6c74a19f4c4f3b02798d81da777812594a3e9
Jan-Kees van Andel <jankeesvanandel@gmail.com>
2015-11-11 14:51:41 +0100

    Increased amount of shown top talk results

--- app/scripts/wall/voting/votingservice.js
+++ app/scripts/wall/voting/votingservice.js
@@ -1,7 +1,7 @@
 wallApp.factory('VotingService', ['$http', '$q', function($http, $q) {
 
     var dayNumberToName = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
-    var topOfWeekUrl = 'https://api-voting.devoxx.com/DV15/top/talks?limit=5';
+    var topOfWeekUrl = 'https://api-voting.devoxx.com/DV15/top/talks?limit=10';
 
     return {
         topOfWeek: function() {
@@ -19,7 +19,7 @@ wallApp.factory('VotingService', ['$http', '$q', function($http, $q) {
             var defer = $q.defer();
 
             var dayNumber = new Date().getDay();
-            var topOfDayUrl = 'https://api-voting.devoxx.com/DV15/top/talks?limit=5&day=' + dayNumberToName[dayNumber];
+            var topOfDayUrl = 'https://api-voting.devoxx.com/DV15/top/talks?limit=10&day=' + dayNumberToName[dayNumber];
 
             $http.get(topOfDayUrl).success(function(data, status, headers, config) {
                 defer.resolve(data);
