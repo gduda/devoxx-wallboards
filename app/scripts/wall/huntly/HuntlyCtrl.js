0b28194ff9bfb207ab7da980c1c01dff9d817558
Jan-Kees van Andel <jankeesvanandel@gmail.com>
2016-11-06 18:14:23 +0100

    Fixed layout glitch for Huntly

--- app/scripts/wall/huntly/HuntlyCtrl.js
+++ app/scripts/wall/huntly/HuntlyCtrl.js
@@ -45,11 +45,28 @@ wallApp.controller('HuntlyCtrl', function ($scope, $interval, HuntlyService) {
     }
 
     function updateModels(response) {
-        return response.data.slice(0, 6);
+        var a = response.data.slice(0, 6);
+        // a.push({username: "BPB1", credits: 1375});
+        // a.push({username: "BPB2", credits: 374});
+        // a.push({username: "BPB3", credits: 372});
+        // a.push({username: "BPB4", credits: 371});
+        // a.push({username: "BPB5", credits: 3});
+        // a.push({username: "BPB6", credits: 2});
+        // a.push({username: "BPB7", credits: 1});
+        return a;
     }
 
     function handleError(response) {
-        console.log('An error occurred retrieving Huntly data: ' + response);
+        console.log('An error occurred retrieving Huntly data: ', response);
+        // var a = [];
+        // a.push({username: "BPB1", credits: 1375});
+        // a.push({username: "BPB2", credits: 374});
+        // a.push({username: "BPB3", credits: 372});
+        // a.push({username: "BPB4", credits: 371});
+        // a.push({username: "BPB5", credits: 3});
+        // a.push({username: "BPB6", credits: 2});
+        // a.push({username: "BPB7", credits: 1});
+        // return a;
     }
 
     this.retrieveLeaderboard = function() {
