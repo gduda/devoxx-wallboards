3dd14047d826a1c01d1fdd0b32e2a042fceaa3eb
Jan-Kees van Andel <jankeesvanandel@gmail.com>
2016-11-06 10:40:43 +0100

    Added batman emoji and moved to 5 tweets

--- app/scripts/wall/utilities.js
+++ app/scripts/wall/utilities.js
@@ -1,5 +1,4 @@
 'use strict';
-/* exported day */
 /* exported getCurrentTime */
 Date.prototype.before = function(other) {
     return this.compareTo(other) < 0;
@@ -14,16 +13,7 @@ Date.prototype.withDate = function(date) {
     return this;
 };
 
-function day(date) {
-    var startOfEvent = new Date(2014, 10, 10);
-    var theDay = date === undefined ? Date.today() : date;
-    var diff = (theDay.getDayOfYear() + 1) - startOfEvent.getDayOfYear();
-    var eventDay = diff >= 1 && diff <= 5 ? diff : 1; // Default to day1 to show something
-    console.log('Calculated event day: ', eventDay);
-    return eventDay;
-}
-
 function getCurrentTime() {
-    return new Date('November 9, 2016 10:24:00');
-    //TODO: Set to correct date: return new Date();
+    //return new Date('November 9, 2016 10:24:00');
+    return new Date();
 }
